cf750c53de6286ce962f6124d2f87a1af3be76db
DevAbidHasan <abidhasanplabon80@gmail.com>
2025-09-08 23:16:30 +0600

    added vercel.json file to deploy server in vercel

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require('express');
 require('dotenv').config()
+const express = require('express');
+
 const cors =require('cors');
 const app = express ();
 const port = process.env.PORT || 3000;
@@ -20,7 +21,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const plantCollection = client.db("plantDB").collection("plants");
 
@@ -87,8 +88,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
